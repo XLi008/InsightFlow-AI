c078085f2b535cabdb4f53988f5b8773d673ef58
Brian Louis <brianlouis.salinas116@gmail.com>
2024-01-28 16:54:50 -0700

    Created a navigation

---
+++ frontend/animation.js
@@ -0,0 +1,1 @@
+// var aniamtion = addEventListener.apply # test ad
--- venv/django.py
+++ venv/django.py
@@ -1,4 +1,13 @@
 import pandas
 import numpy
+import sklearn
 
-import lua
+
+
+
+data = pandas.DataFrame(None)
+
+if not isinstance:
+    then_data = data 
+
+    # P/E=\frac{\text{Share price}}{\text{Earnings per share}}

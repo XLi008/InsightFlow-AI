33b26f18db4e57650a6e1ea088fb2ecb86e6298e
Brian Louis <brianlouis.salinas116@gmail.com>
2024-02-03 23:34:38 -0700

    fixed the error in linear-regression data and created a navigation bar in Home.html

--- venv/linear-regression.py
+++ venv/linear-regression.py
@@ -1,22 +1,17 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import yfinance
 import xgboost as xgb
 import yfinance as yt
-
-data = pd.read_csv("indexData.csv", index_col=0)
-
-data.head()
 
 user = input("Choose a Stock Symbol: ")
 
 stock = yfinance.download(user, start='2000-01-01', end='2024-01-01')
 
-graph = plt.plot(stock)
 
 
-def plot():
-    plt.figure(figsize=(100, 100))
+def plot(user):
+    plt.figure()
     plt.plot(stock)
     plt.title(f"{user} Stock over Time")
     plt.xlabel("Date")
@@ -24,6 +19,4 @@ def plot():
     plt.show()
 
 
-
-
-
+plot(stock)

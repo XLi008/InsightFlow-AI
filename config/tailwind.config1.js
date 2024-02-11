912cf4ef6e83b232c06ec8a1ad7903d60c44439a
Brian Louis <brianlouis.salinas116@gmail.com>
2024-02-10 22:12:00 -0700

    changed the web designing,

--- config/tailwind.config1.js
+++ config/tailwind.config1.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
-
